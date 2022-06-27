717349a80d7c14be0712af52164d68b8a66b119e
Oualid <oualid.najah@gmail.com>
2022-06-27 10:03:56 +0200

    unstage image

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,10 +4,12 @@ module.exports=(req,res,next)=>{
 // authentification pour chaque requete afin de verifier si le userid correspond au userid qui a envoyé la requete indispensable pour sécurisé nos requetes 
     try{
 const token=req.headers.authorization.split(' ')[1];
+//Decodage
 const decodedToken=jwt.verify(token, 'udl*VFMnxp5Crly-({')
+// encodage
 const userId=decodedToken.userId
 //req.body.userId &&
-req.auth={userId:userId}
+
 if (req.body.userId && req.body.userId !== userId){
     throw 'invalid user ID'
 }else{
